a56de728bce4bd315c1b92870881706147c4f3d2
MakMongia <85044367+MakMongia@users.noreply.github.com>
2024-03-29 09:41:11 +1100

    Home
    Home coponent is done and moved other assets under src

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.test';
 import Header from './Component/Header/Header';
+import Home from './Component/Main/Home';
 
 function App() {
   return (
     <div className="App">
       <h1> Elite Hoops Club</h1>
-      <Header/>
+      <Header />
+      <Home />
+      
       </div>
   );
 }
--- my-app/src/Component/Main/Home.js
+++ my-app/src/Component/Main/Home.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import Banner1 from '../img/Banner1.png';
-import Banner2 from '../img/Banner2.png';
-import Banner3 from '../img/Banner3.png';
-import '../styles/Home.css';
+import React, { useState } from 'react';
+import Banner1 from '../../assets/images/Banner1.png';
+import Banner2 from '../../assets/images/Banner2.png';
+import Banner3 from '../../assets/images/Banner3.png';
+
 
 function Home() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 2 ? 0 : prevSlide + 1));
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? 2 : prevSlide - 1));
+  };
+
   return (
     <section id="home" className="home-section">
       <div className="carousel-container">
-        <div className="carousel-item">
-          <img src={Banner1} alt="Banner Image 1" />
+        <div className={currentSlide === 0 ? 'carousel-item active' : 'carousel-item'}>
+          <img src={Banner1} alt="Image 1" />
         </div>
-        <div className="carousel-item">
-          <img src={Banner2} alt="Banner Image 2" />
+        <div className={currentSlide === 1 ? 'carousel-item active' : 'carousel-item'}>
+          <img src={Banner2} alt="Image 2" />
         </div>
-        <div className="carousel-item">
-          <img src={Banner3} alt="Banner Image 3" />
+        <div className={currentSlide === 2 ? 'carousel-item active' : 'carousel-item'}>
+          <img src={Banner3} alt="Image 3" />
         </div>
       </div>
-      <button id="prevBtn">Previous</button>
-      <button id="nextBtn">Next</button>
+
+      <button id="prevBtn" onClick={prevSlide}>Previous</button>
+      <button id="nextBtn" onClick={nextSlide}>Next</button>
     </section>
   );
 }
