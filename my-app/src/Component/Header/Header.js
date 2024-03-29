e64197400277cadbcee2243e66a433bb60f5bd46
MakMongia <85044367+MakMongia@users.noreply.github.com>
2024-03-29 13:27:11 +1100

    Header CSS improved
    Header CSS is done, fixed the header and made some improvements

--- my-app/src/Component/Header/Header.js
+++ my-app/src/Component/Header/Header.js
@@ -3,8 +3,6 @@ import logo from '../../assets/images/Logo.png';
 import './Header.css';
 
 function Header() {
-
-
   return (
     <header>
       <nav id="navbar">
@@ -15,7 +13,7 @@ function Header() {
           <li><a href="#events">Events</a></li>
           <li><a href="#news">News</a></li>
           <li><a href="#merch">Merch</a></li>
-          <li><a href="#contactus">Contact Us</a></li>
+          <li><a href="#contactus">ContactUs</a></li>
           <li><img src={logo} alt="Logo" /></li>
        </ul>
       </nav>
