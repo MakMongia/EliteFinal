d01eb57609597a9f13e60f37a796e17dbf6b1968
MakMongia <85044367+MakMongia@users.noreply.github.com>
2024-03-30 14:57:21 +1100

    Footer Done
    Footer done

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 # fix merch clickable button and fix its css
 # make tables background brighter
 # caoursel position line under it 
+# fix the linking, all the links are opening a bit below
--- my-app/src/Component/Footer/Footer.js
+++ my-app/src/Component/Footer/Footer.js
@@ -21,6 +21,7 @@ function Footer() {
         </div>
       </div>
       <div className="footer-content">
+        <img src={logo} alt="Logo" className="footer-logo" />
         <div className="contact-info">
           <p>Elite Hoops Club</p>
           <p>15 Springvale Drive</p>
@@ -28,12 +29,12 @@ function Footer() {
           <p>Phone No. - +61 444444444</p>
           <p>email - info@Elitehc.com.au</p>
         </div>
-        <img src={logo} alt="Logo" className="footer-logo" />
       </div>
       <div className="policies">
-        <span className="footer-link" onClick={() => window.location.href = "privacy-statement-url"}>Privacy Statement</span>
-        <span className="footer-link" onClick={() => window.location.href = "terms-and-conditions-url"}>Terms and Conditions</span>
+       <span className="footer-link">Privacy Statement</span>
+       <span className="footer-link">Terms and Conditions</span>
       </div>
+
     </footer>
   );
 }
